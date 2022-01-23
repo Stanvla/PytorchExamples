3f3c59ed06828cdbcc9ed6487bf4e02337f976d8
Vladislav Stankov <stankov344@gmail.com>
2022-01-23 09:34:19 +0100

    added init script for installing packeges on the remote server

---
+++ init.sh
@@ -0,0 +1,6 @@
+conda install -y -c conda-forge notebook pandas matplotlib icecream pytorch-lightning
+conda install -y -c pytorch torchaudio torchtext
+
+# conda install -y -c conda-forge gdown
+# gdown https://drive.google.com/uc?id=1d3Zb9heon2FFv3rZEW2YaI8F46aSkXba
+# tar -xf cv-corpus-7.0-2021-07-21-cs.tar.gz
