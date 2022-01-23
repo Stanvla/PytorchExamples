923dd3f31a14190c897d1a9168d8165bae84248f
Vladislav Stankov <stankov344@gmail.com>
2022-01-23 09:50:12 +0100

    upadted init script

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
-conda install -y -c conda-forge notebook pandas matplotlib icecream pytorch-lightning
+conda install -y -c conda-forge notebook pandas matplotlib icecream pytorch-lightning youtokentome datasets transformers tokenizers
 conda install -y -c pytorch torchaudio torchtext
+jupyter notebook --no-browser --ip=0.0.0.0 --port=8080 --allow-root
 
+
+# to download things from gdrive
 # conda install -y -c conda-forge gdown
 # gdown https://drive.google.com/uc?id=1d3Zb9heon2FFv3rZEW2YaI8F46aSkXba
 # tar -xf cv-corpus-7.0-2021-07-21-cs.tar.gz
