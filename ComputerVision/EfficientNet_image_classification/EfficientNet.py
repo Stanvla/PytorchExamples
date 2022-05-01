765944c07a8d7f2c94408005bdabcccf851450a4
Vladislav Stankov <stankov344@gmail.com>
2022-05-01 23:51:32 +0200

    Implemented dataset loading and updated transforms according to the paper (now using AutoAugment). Started pytorch lightning implementation

--- ComputerVision/EfficientNet_image_classification/EfficientNet.py
+++ ComputerVision/EfficientNet_image_classification/EfficientNet.py
@@ -1,13 +1,21 @@
+# %%
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as T
-
-from Augmentation import PerformAtMostN, MyRandomAdjustSharpness
-from collections import OrderedDict, namedtuple
+# from Augmentation import PerformAtMostN, MyRandomAdjustSharpness
 from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader, Subset
+
+import pytorch_lightning as pl
+
+from sklearn.model_selection import train_test_split
+
 import os
+import math
 from icecream import ic
+from typing import Optional
+from collections import OrderedDict, namedtuple
 
 
 def swish(t):
@@ -102,10 +110,11 @@ class SqueezeAndExcitation(nn.Module):
 
 class MBConv(nn.Module):
     """Even though in paper the block is denoted as MBConv, in the text they write that they also used squeeze-and-excitation subblock."""
-    def __init__(self, exp_factor, kernel_size, in_channels, out_channels, stride, bn_moment, padding, stochastic_depth):
+    def __init__(self, exp_factor, kernel_size, in_channels, out_channels, stride, bn_moment, padding, stochastic_depth, layer_idx):
         super(MBConv, self).__init__()
         self.stochastic_depth_prob = stochastic_depth
         self.stride = stride
+        self.layer_idx = layer_idx
 
         exp_channels = exp_factor * out_channels
         self.conv_bn_swish1 = ConvBNSwish(
@@ -148,6 +157,7 @@ class MBConv(nn.Module):
         # can perform skip connection, squeeze-excitation and stochastic depth
         # only if stride == 1 and number of input and output channels is equal (then self.se is not None)
         if self.stride == 1 and self.se is not None:
+            print(f'{self.layer_idx} skip connection')
             weights = self.se(output)
             # weights.shape = [batch, channels]
             # output.shape = [batch, channels, H, W]
@@ -155,6 +165,7 @@ class MBConv(nn.Module):
 
             # stochastic depth
             if self.training:
+                print(f'{self.layer_idx} stoch depth')
                 survived = torch.rand(output.shape[0]) < self.stochastic_depth_prob
                 survived = survived.type(torch.float).view(survived.shape[0], 1, 1, 1)
                 output = (survived / self.stochastic_depth_prob) * survived
@@ -166,7 +177,7 @@ class MBConv(nn.Module):
 
 class MBConvBlock(nn.Module):
     """Just a list of MBConvs, with careful handling of first MBConv, should take care of spatial resolution and channels."""
-    def __init__(self, layers_cnt, stride, in_channels, out_channels, kernel_size, bn_moment, exp_factor, stochastic_depths, padding):
+    def __init__(self, layers_cnt, stride, in_channels, out_channels, kernel_size, bn_moment, exp_factor, stochastic_depths, padding, layers_offset):
         super(MBConvBlock, self).__init__()
         self.layers_cnt = layers_cnt
         self.stride = stride
@@ -187,6 +198,7 @@ class MBConvBlock(nn.Module):
                 bn_moment=bn_moment,
                 padding=padding,
                 stochastic_depth=stochastic_depths[0],
+                layer_idx=layers_offset,
             )
         ]
         # the rest of the block will work with fixed space resolution and fixed number of channels
@@ -201,6 +213,7 @@ class MBConvBlock(nn.Module):
                     bn_moment=bn_moment,
                     padding='same',
                     stochastic_depth=stochastic_depths[i],
+                    layer_idx=layers_offset + i,
                 )
             )
         self.mb_convs = nn.ModuleList(self.mb_convs)
@@ -218,7 +231,7 @@ class MBConvBlock(nn.Module):
 
 class EfficientNet(nn.Module):
     """Custom implementation of EfficientNetB0 according to the paper."""
-    def __init__(self, arch_params, bn_moment, do, n_targets, head_features):
+    def __init__(self, arch_params, bn_moment, do, n_targets, head_features, stochastic_depth):
         super(EfficientNet, self).__init__()
 
         first_conv = ConvBNSwish(
@@ -232,9 +245,7 @@ class EfficientNet(nn.Module):
         )
         blocks_lst = [(first_conv.get_name(), first_conv)]
 
-        stochastic_depth = torch.linspace(0, 0.5, sum([p.block_size for p in arch_params]) + 1)
-        stochastic_depth_idx = 1
-
+        layers_offset = 1
         for p in arch_params[1:]:
             block = MBConvBlock(
                 layers_cnt=p.block_size,
@@ -244,11 +255,12 @@ class EfficientNet(nn.Module):
                 kernel_size=p.kernel_size,
                 bn_moment=bn_moment,
                 exp_factor=p.exp_factor,
-                stochastic_depths=stochastic_depth[stochastic_depth_idx: stochastic_depth_idx + p.block_size],
-                padding=p.padding
+                stochastic_depths=stochastic_depth,
+                padding=p.padding,
+                layers_offset=layers_offset,
             )
             blocks_lst.append([block.get_name(), block])
-            stochastic_depth_idx += p.block_size
+            layers_offset += p.block_size
 
         self.block_dct = OrderedDict(blocks_lst)
         self.conv_head = ConvBNSwish(
@@ -268,7 +280,7 @@ class EfficientNet(nn.Module):
         print(output.shape)
         for name, block in self.block_dct.items():
             output = block(output)
-            print(f'{name:50} {output.shape}')
+            # print(f'{name:50} {output.shape}')
         output = self.conv_head(output)
         output = torch.mean(output, dim=(2, 3), keepdim=False)
         output = self.drop_out(output)
@@ -276,17 +288,171 @@ class EfficientNet(nn.Module):
         return self.linear(output)
 
 
+class EfficientNetPL(pl.LightningDataModule):
+    def __init__(self, eff_net, lr, weight_decay, momentum):
+        super(EfficientNetPL, self).__init__()
+        self.eff_net = eff_net
+        self.init_lr = lr
+        self.weight_decay = weight_decay
+        self.momentum = momentum
+        self.optim_eps = 0.001
+
+    def forward(self, batch):
+        return self.eff_net(batch)
+
+    def _perform_step(self, batch):
+        images, targets = batch['images'], batch['targets']
+        outputs = self(images)
+
+        loss = F.cross_entropy(outputs, targets)
+
+        predictions = outputs.argmax(dim=1)
+        correct = torch.sum(predictions == targets).item()
+        acc = correct / targets.shape[0]
+
+        return loss, acc, targets[0]
+
+    def training_step(self, batch, batch_idx):
+        loss, acc, batch_size = self._perform_step(batch)
+        return dict(loss=loss, acc=acc, batch_size=batch_size)
+
+    def validation_step(self, batch, batch_idx):
+        loss, acc, batch_size = self._perform_step(batch)
+        return dict(loss=loss, acc=acc, batch_size=batch_size)
+
+    def test_step(self, batch, batch_idx):
+        loss, acc, batch_size = self._perform_step(batch)
+        return dict(loss=loss, acc=acc, batch_size=batch_size)
+
+    def configure_optimizers(self):
+        pass
+
+    def training_epoch_end(self, outputs):
+        pass
+
+    def validation_epoch_end(self, outputs):
+        pass
+
+    def test_epoch_end(self, outputs):
+        pass
+
+
+class Cifar10PL(pl.LightningDataModule):
+    def __init__(self, path, train_perc, batch_size, seed, train_tr, test_tr):
+        super(Cifar10PL, self).__init__()
+        self.path = path
+        self.bs = batch_size
+        self.num_workers = (os.cpu_count() - 2) // torch.cuda.device_count()
+        self.train_perc = train_perc
+        self.seed = seed
+        self.train_tr = train_tr
+        self.test_tr = test_tr
+        # self.train, self.val, self.test = None, None, None
+
+    def prepare_data(self):
+        """things to do on 1 GPU/TPU not on every GPU/TPU in distributed mode, e.g. downloading data only once"""
+        if not os.path.isdir(os.path.join(self.path, 'train')):
+            CIFAR10(os.path.join(os.getcwd(), 'train'), download=True)
+        if not os.path.isdir(os.path.join(self.path, 'test')):
+            CIFAR10(os.path.join(os.getcwd(), 'train'), train=False, download=True)
+
+    def setup(self, stage: Optional[str] = None):
+        """(things to do on every accelerator in distributed mode) called on every process in DDP."""
+        train_and_val = CIFAR10(os.path.join(os.getcwd(), 'train'))
+
+        train_indices, val_indices = train_test_split(
+            range(len(train_and_val)),
+            train_size=self.train_perc,
+            random_state=self.seed
+        )
+
+        train_data = CIFAR10(os.path.join(os.getcwd(), 'train'), transform=self.train_tr)
+        self.train = Subset(train_data, train_indices)
+
+        val_data = CIFAR10(os.path.join(os.getcwd(), 'train'), transform=self.test_tr)
+        self.val = Subset(val_data, val_indices)
+
+        self.test = CIFAR10(os.path.join(os.getcwd(), 'test'), transform=self.test_tr)
+
+    def get_dataloader(self, dataset):
+        return DataLoader(
+            dataset,
+            batch_size=self.bs,
+            collate_fn=collate_fn,
+            pin_memory=True,
+            num_workers=self.num_workers
+        )
+
+    def train_dataloader(self):
+        return self.get_dataloader(self.train)
+
+    def val_dataloader(self):
+        return self.get_dataloader(self.val)
+
+    def test_dataloader(self):
+        return self.get_dataloader(self.test)
+
+
+def collate_fn(lst):
+    images, targets = [], []
+    for i, t in lst:
+        images.append(i)
+        targets.append(t)
+    return dict(images=torch.stack(images), targets=torch.tensor(targets))
+
+
+class TrainTransform:
+    def __init__(self, trainsforms):
+        self.trans = trainsforms
+
+    def __call__(self, image):
+        output = image
+        for t in self.trans:
+            output = t(output)
+        return output
+
+
+# %%
 if __name__ == '__main__':
+    # %%
+
     image_augmentation = None
     new_resolution = 224
 
-    to_tensor_tr = T.ToTensor()
-    cifar10_train = CIFAR10(os.path.join(os.getcwd(), 'train'), download=True)
-    xs = torch.stack([to_tensor_tr(cifar10_train[i][0]) for i in range(10)])
+    train_transform = TrainTransform([
+        T.AutoAugment(T.AutoAugmentPolicy.CIFAR10),
+        T.ToTensor(),
+        T.Resize(new_resolution),
+    ])
 
+    test_transform = T.Compose([
+        T.ToTensor(),
+        T.Resize(new_resolution)
+    ])
+
+    dataset = Cifar10PL(
+        path=os.getcwd(),
+        train_perc=0.7,
+        batch_size=20,
+        seed=0xDEAD,
+        train_tr=train_transform,
+        test_tr=test_transform
+    )
+    dataset.setup()
+
+    train_loader = dataset.train_dataloader()
+    for i, b in enumerate(train_loader):
+        if i == 5:
+            break
+        ic(b)
+
+
+    # %%
+    # named tuple represents parameters for each block of Efficient net
     Block = namedtuple('Block', 'out_res block_size channels_in channels_out kernel_size stride exp_factor padding')
     # out_res is the output resolution it is for debugging
-    # first dict is for the first convolution
+    # first dict is for the first convolution and not for the block
+
     params = [
         Block(out_res=112, block_size=0, channels_in=3,   channels_out=32,  kernel_size=3, stride=2, exp_factor=0, padding=1),
         Block(out_res=112, block_size=1, channels_in=32,  channels_out=16,  kernel_size=3, stride=1, exp_factor=1, padding='same'),
@@ -305,10 +471,9 @@ if __name__ == '__main__':
         n_targets=10,
         head_features=1280
     )
-    # output_resolutions = [(112,112), (112,112), (56,56), (28,28), (14,14), (14,14), (7,7), (7,7)]
 
     resize_tr = T.Resize(new_resolution)
-    model(resize_tr(xs))
+    # model(resize_tr(xs))
 
 
     # color_and_sharpness_tr = PerformAtMostN(
